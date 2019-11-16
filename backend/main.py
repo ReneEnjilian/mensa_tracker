305b8a28e42d0095105300029c8ab35c31dbd34c
Rene Enjilian <enjilianrene@gmail.com>
2019-11-16 17:16:00 +0100

    adding sqllite

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
-from app import app
+from app import app, db
 from routes import routes
 
 
 
 app.register_blueprint(routes)
+db.create_all()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
