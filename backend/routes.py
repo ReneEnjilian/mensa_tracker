407a1868ba0dc5610823bb839f18b61f198e52ba
Rene Enjilian <enjilianrene@gmail.com>
2019-11-16 16:04:28 +0100

    adding docker

--- backend/routes.py
+++ backend/routes.py
@@ -8,7 +8,15 @@ from app import app
 def hello():
     return "this is a test"
 
-@app.route("/test", methods=['GET'])
-def scrap():
+@app.route("/cafeterias", methods=['GET'])
+def refresh_all_cafeterias():
     start_scrapers()
     return "success"
+
+@app.route("/cafeterias/<int:id>", methods=['GET'])
+def load_cafeteria(id):
+    pass
+
+@app.route("/cafeterias/<int:id>/<day>", methods=['GET'])
+def load_cafeteria_for_day(id, day):
+    pass
